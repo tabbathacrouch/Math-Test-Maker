f6919e5d8fd72c358de5097bd7bb3ccf9d1abd75
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2021-04-17 13:57:20 -0500

    feat: adds apollo server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,37 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { resolvers } = require('./resolvers/test');
 // Suh dude
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+// const resolvers = {
+//   Query: {
+//     books: () => books,
+//   },
+// };
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log('server started at: ', url);
+});

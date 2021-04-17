3160eada8118381b175ca9b5741060360c50fab2
Tabbatha Crouch <16652968+tabbathacrouch@users.noreply.github.com>
2021-04-17 14:26:12 -0500

    MTM-3 connect client and server (#4)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from "@apollo/client";
 
+const BOOK_QUERY = gql`
+  query GetBooks {
+    books {
+      title
+      author
+    }
+  }
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { data, loading, error } = useQuery(BOOK_QUERY);
+  console.log(data);
+  return <div>Hello World</div>;
 }
 
 export default App;

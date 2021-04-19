7688a64790b5b7c0720f1adf39b41c560b3e7a47
tabbathacrouch <tabbathacrouch@gmail.com>
2021-04-18 21:57:15 -0500

    MTM-4 add react router dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const BOOK_QUERY = gql`
   query GetBooks {
@@ -11,7 +12,21 @@ const BOOK_QUERY = gql`
 function App() {
   const { data, loading, error } = useQuery(BOOK_QUERY);
   console.log(data);
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Router>
+        
+        <Switch>
+          <Route path="/register">
+            register page
+          </Route>
+          <Route path="/signin">
+            sign in page
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

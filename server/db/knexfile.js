80813e5a4ac366f4cc73374460c81ef2f920b156
Liam Hatcher <hatcherpiano@gmail.com>
2021-04-21 21:06:13 -0500

    MTM-4 db env variables (#13)

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,27 +1,28 @@
+const path = require("path");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+}
+
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'mtm',
-      user: 'liamhatcher',
-      password: null
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: null,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   // production: {
   //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
+  //   connection: process.env.DATABASE_URL,
   //   pool: {
   //     min: 2,
   //     max: 10

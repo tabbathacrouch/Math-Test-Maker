592adf6ec57714bea44c4fa58e66eff818b918a2
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2021-04-27 21:43:25 -0500

    feat: adds working jwt resolver for login mutation

--- server/resolvers/mutations.js
+++ server/resolvers/mutations.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken')
 
 const user = require("../dao/user");
 
+const { mockUsers } = require('../data/userData');
+
 module.exports = {
   createUser: async (_, args) => {
     const { fullName, email } = args;
@@ -11,7 +13,14 @@ module.exports = {
     // Successful if no errors
     return { success };
   },
-  // login: (_, args, { user }) => {
-  //   // add logic for loging a user in here
-  // }
+  login: (_, { email, password }) => {
+    const { id, permissions, roles } = mockUsers.find(
+      user => user.email === email && user.password === password
+    );
+    return jwt.sign(
+      { 'https://spaceapi.com/graphql': { roles, permissions } },
+      'asdlkfjasdfajsdfuw',
+      { algorithm: 'HS256', subject: id, expiresIn: '1d' }
+    );
+  }
 };
